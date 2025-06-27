ce73b550b8f901a91e533bb1f524b9b1fe45b049
Daniel Lockyer <daniellockyer@fastmail.com>
2025-06-27 12:01:51 +0200

    Preloaded thumbnail variants to avoid n+1 SQL queries (#471)
    - we can avoid a bunch of DB queries by preloading them in advance
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **Bug Fixes**
    * Improved loading performance for product thumbnails and their image
    variants, ensuring faster and more reliable display of images.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/presenters/creator_home_presenter.rb
+++ app/presenters/creator_home_presenter.rb
@@ -35,7 +35,7 @@ class CreatorHomePresenter
     product_permalinks = top_sales_data.map(&:first)
     products_by_permalink = seller.products
       .where(unique_permalink: product_permalinks)
-      .includes(thumbnail: { file_attachment: :blob })
+      .includes(thumbnail: { file_attachment: { blob: { variant_records: { image_attachment: :blob } } } })
       .index_by(&:unique_permalink)
 
     sales = top_sales_data.map do |p|
